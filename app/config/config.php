203d44778513e7e2936d456b34908517db3c8f8d
MbouRowin <117640285+MbouRowin@users.noreply.github.com>
2023-11-13 14:21:33 +0100

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'backend-opdracht-2');
+define('URLROOT', 'http://backend-opdracht-2.test');
